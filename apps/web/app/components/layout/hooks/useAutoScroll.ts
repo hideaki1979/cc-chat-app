38acac3d1a308054e0ca6075e117670ac1637a18
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-09-01 22:19:30 +0900

    feat: チャット関連画面実装(PR対応6)

--- apps/web/app/components/layout/hooks/useAutoScroll.ts
+++ apps/web/app/components/layout/hooks/useAutoScroll.ts
@@ -1,11 +1,11 @@
 import { useLayoutEffect, useRef } from "react"
 
-export const useAutoScroll = (dependency: number) => {
+export const useAutoScroll = (dependency: unknown) => {
     const ref = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
         // DOM更新後に同期的にスクロール実行
-        ref.current?.scrollIntoView({behavior: 'smooth'});
-    })
-  return ref;
+        ref.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [dependency])
+    return ref;
 }
